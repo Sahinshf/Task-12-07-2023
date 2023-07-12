9acd379affe8971a2e5e40741524e7f586e6696f
Sahinshf <shahinrsha@code.edu.az>
2023-07-12 17:54:17 +0400

    feat dark mode

--- src/layout/Site/Header/Header.jsx
+++ src/layout/Site/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Styles from "./Header.module.css";
+import Darkmode from "darkmode-js";
 
 const Header = () => {
   return (
@@ -7,7 +8,12 @@ const Header = () => {
       <div className={`${Styles.app__header} container`}>
         <h1>Where in the world ?</h1>
         <div>
-          <p className={Styles.app__header__darkmode}>Dark Mode</p>
+          <p
+            className={Styles.app__header__darkmode}
+            onClick={() => new Darkmode().showWidget}
+          >
+            Dark Mode
+          </p>
         </div>
       </div>
     </div>
--- src/pages/Site/CountryDetail/CountryDetail.jsx
+++ src/pages/Site/CountryDetail/CountryDetail.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Styles from "./CountryDetail.module.css";
 import { useParams } from "react-router-dom";
-import { act } from "react-dom/test-utils";
 import axios from "axios";
 
 const CountryDetail = () => {
@@ -14,64 +13,59 @@ const CountryDetail = () => {
       .then((response) => {
         setCountry(response.data[0]);
       });
-  }, []);
-
-  //className={`${}`}
+  }, [name]);
 
   return (
-    {country.flags === undefined ? <div></div> :
-    <div className={`${Styles.app__countryDetail__section} container`}>
-    {console.log(country.flags)}
-    <div className={`${Styles.app__countryDetail__image} col-md-6`}>
-      <img src={country.flags.png}  />
-    </div>
-    <div className={`${Styles.app__countryDetail__description} col-md-6`}>
-      <h2>{country.name.common}</h2>
-      <div className={Styles.app__countryDetail__information}>
-        <div>
-          <div>
-            <h6>Native Name</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Population</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Region</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Sub Region</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Capital</h6>
-            <p></p>
+    <div>
+      {country.flags === undefined ? (
+        <div></div>
+      ) : (
+        <div className={`${Styles.app__countryDetail__section}  container`}>
+          {console.log(country.currencies)}
+          <div className=" justify-content-between align-items-center row">
+            <div className={`${Styles.app__countryDetail__image} col-md-6`}>
+              <img src={country.flags.png} alt="Country Flag" />
+            </div>
+            <div
+              className={`${Styles.app__countryDetail__description} col-md-6`}
+            >
+              <h2>{country.name.common}</h2>
+              <div className={Styles.app__countryDetail__information}>
+                <div>
+                  <div>
+                    <h6>Population : </h6>
+                    <p>{country.population}</p>
+                  </div>
+                  <div>
+                    <h6>Regio : </h6>
+                    <p>{country.region}</p>
+                  </div>
+                  <div>
+                    <h6>Sub Region : </h6>
+                    <p>{country.subregion}</p>
+                  </div>
+                  <div>
+                    <h6>Capital : </h6>
+                    <p>{country.capital[0]}</p>
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <h6>Top Level Domain : </h6>
+                    <p>{country.tld}</p>
+                  </div>
+                  <div>
+                    <h6>Languages : </h6>
+                    <p>{Object.values(country.languages).join(", ")}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div>
-          <div>
-            <h6>Top Level Domain</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Currencies</h6>
-            <p></p>
-          </div>
-          <div>
-            <h6>Languages</h6>
-            <p></p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
-  </div>
-  }
-
-
-    
   );
 };
 
